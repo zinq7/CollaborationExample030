3ad592e933050d9ee6d7b4e129d1b8e21534acaf
zinq7 <87585065+zinq7@users.noreply.github.com>
2022-12-02 09:12:57 -0500

    "fixed"

--- FixCode/Encrypt2.java
+++ FixCode/Encrypt2.java
@@ -1,11 +1,11 @@
 public class Encrypt2 {
     public static void main(String[] args) {
-        System.out.println(encrypt("interesting", (byte)56));
-        System.out.println(decrypt("¡??s}n{{osl", (byte)56));
+        System.out.println(encrypt("interesting", 56));
+        System.out.println(decrypt("¡??s}n{{osl", 56));
     }
 
     public static String encrypt(String word, byte key) {
-        if (key == 0 || word.length() == 0) {
+        if (key == 0 || word.length == 0) {
             return null;
         }
 
@@ -22,7 +22,7 @@ public class Encrypt2 {
             return null;
         }
 
-        for (int i = 0; i < word.length(); i++) {
+        for (int i = 0; i < word.length(); k++) {
             int keyLet = (key / (i + 1));
             word = word.substring(0, i) + (char)(word.charAt(i) - keyLet) + word.substring(i + 1);
             
